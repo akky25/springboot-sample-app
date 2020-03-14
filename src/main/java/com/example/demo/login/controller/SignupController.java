df1b1dd800bb37292b0a46f3fdc011b061c20eb6
akky25 <ry.akita1991@gmail.com>
2020-03-15 01:34:51 +0900

    Cp6.1終了 login&singupのコントローラ&View作成

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -1,1 +1,51 @@
 package com.example.demo.login.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * SignupController
+ */
+@Controller
+public class SignupController {
+
+    private Map<String, String> radioMarriage;
+
+    private Map<String, String> initRadioMarrige() {
+
+        Map<String, String> radio = new LinkedHashMap<>();
+
+        radio.put("既婚", "false");
+        radio.put("未婚", "true");
+
+        return radio;
+    }
+    
+    /**
+     * ユーザ登録画面のGET用コントローラ
+     */
+    @GetMapping("/signup")
+    public String getSignUp(Model model) {
+
+        // ラジオボタンの初期化メソッド呼び出し
+        radioMarriage = initRadioMarrige();
+
+        // ラジオボタン用のMapをModelに登録
+        model.addAttribute("radioMarriage", radioMarriage);
+
+        return "login/signup";
+    }
+
+    /**
+     * ユーザー登録画面のPOSTコントローラ
+     */
+    public String postSignUp(Model model) {
+        
+        return "redirect:/login";
+
+    }
+}
